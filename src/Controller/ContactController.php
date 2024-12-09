92e9a16b5331e19da09fca057b4eff217655def7
keuss le mojo <julio.nathan47@gmail.com>
2024-12-09 10:38:40 +0100

    contact mail

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,27 +8,36 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
 
     #[Route('/contact', name: 'app_contact')]
-    public function index(EntityManagerInterface $entityManager, Request $request): Response
+    public function index(MailerInterface $mailer, Request $request): Response
     {
         $contact = new Contact();
+
         $contact_form = $this->createForm(ContactType::class, $contact);
 
         $contact_form->handleRequest($request);
 
-        if ($contact_form->isSubmitted() && $contact_form->isValid()) {
+        if ($contact_form->isSubmitted()) {
 
-            $entityManager->persist($contact);
-            $entityManager->flush();
+            $email = new Email();
+
+            $emailTemplate = $this->renderView('contact/contact_send.html.twig', [
+                'contact' => $contact,
+            ]);
 
             $this->addFlash('success', 'Votre message a bien été envoyé.');
 
-            return $this->redirectToRoute('contact_send', ['id' => $contact->getId()]);
+            $mailer->send($email->from('test@top.com')
+                ->to('contact@top.com')
+                ->subject('Une demande de contact a été faite.')
+                ->html($emailTemplate));
         }
 
         return $this->render('contact/index.html.twig', [
